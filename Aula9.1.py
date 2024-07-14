3ee88145f369547c09a4c5f885608188bfebc445
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-14 11:05:51 -0300

    aula09

--- Aula9.1.py
+++ Aula9.1.py
@@ -1,3 +1,7 @@
 frase = 'Curso em Video Python'
 frase=frase.replace('Python','Android')
 print(frase)
+print(frase.split())#cria uma lista com separador
+dividido=frase.split()
+print(dividido[0])#mostra a primeira string
+print(dividido[2][3])# pega a primeira
---
+++ aula9.2.py
@@ -0,0 +1,4 @@
+frase = 'Curso em Video Python'
+dividido=frase.split()
+print(dividido[2])#mostra a primeira string
+print(dividido[2][3])# mostra a segunda palavra e exibe o terceiro caractere
