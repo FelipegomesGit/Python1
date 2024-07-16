0efb9ebe338a13580e53e0140ed8972c3ff27446
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-16 20:03:05 -0300

    atualizando

--- desafio026.py
+++ desafio026.py
@@ -5,5 +5,7 @@
 frase=str(input('Digite um frase: ')).strip()
 tamanho = frase.upper().count('A')
 comeco=frase.upper().find('A')
+fim=frase.upper().rfind('A')
 print ('A letra "A" aparece {} vezes. '.format(tamanho))
 print ('A letra A pararece na posição {} pela primeira vez'.format(comeco))
+print ('A letra A pararece na posição {} pela ultima vez'.format(fim))
