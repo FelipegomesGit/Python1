99eb1d26057a4b5fa154c157c5fa5c1f3ff2cb66
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-17 20:18:19 -0300

    aula 10

---
+++ aula10.py
@@ -0,0 +1,4 @@
+nome=str(input('Digite seu nome: '))
+if nome.upper() =='FELIPE':
+    print('Que nome top!')
+print('Bom dia, {}'.format(nome))
--- desafio026.py
+++ desafio026.py
@@ -5,7 +5,7 @@
 frase=str(input('Digite um frase: ')).strip()
 tamanho = frase.upper().count('A')
 comeco=frase.upper().find('A')+1
-fim=(frase.upper().rfind('A')+1)
+fim=frase.upper().rfind('A')+1
 print ('A letra "A" aparece {} vezes. '.format(tamanho))
 print ('A letra A pararece na posição {} pela primeira vez'.format(comeco))
 print ('A letra A pararece na posição {} pela ultima vez'.format(fim))
