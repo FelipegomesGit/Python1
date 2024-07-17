ec011dd93afcb309ef25cd1a56880e2c28bac25a
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-17 19:44:13 -0300

    feitos

--- desafio026.py
+++ desafio026.py
@@ -4,8 +4,8 @@
 # em que posição ela aparece a ultima vez
 frase=str(input('Digite um frase: ')).strip()
 tamanho = frase.upper().count('A')
-comeco=frase.upper().find('A')
-fim=frase.upper().rfind('A')
+comeco=frase.upper().find('A')+1
+fim=(frase.upper().rfind('A')+1)
 print ('A letra "A" aparece {} vezes. '.format(tamanho))
 print ('A letra A pararece na posição {} pela primeira vez'.format(comeco))
 print ('A letra A pararece na posição {} pela ultima vez'.format(fim))
