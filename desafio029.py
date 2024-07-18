595c9d34e35150032c591a171b4e6bbb4c33ef2f
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-18 19:10:10 -0300

    desafio29

---
+++ desafio029.py
@@ -0,0 +1,9 @@
+# escreva um programa que leia a velocidade de um carro e se ele ultrapassar 80km informe que ele será multado.
+# a multa vai custar 7 reais por cada km acima
+km=int(input('Digite a velocidade: '))
+if km > 80:
+    print('Você será multado')
+    multa = (km - 80) * 7
+    print ('Sua multa sé de {:.2} reais.'. format (multa))
+else:
+    print("Parabéns você esta no limite correto de velocidade")
