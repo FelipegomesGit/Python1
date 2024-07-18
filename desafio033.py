626511c67e9fbf4ab5c8e679a1169647db946aba
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-18 20:09:45 -0300

    commit

---
+++ desafio033.py
@@ -0,0 +1,18 @@
+# leia 3 numeros e mostre qual o maior e qual o menor
+a=int(input('Informe o primeiro numero: '))
+b=int(input('Informe o segundo numero: '))
+c=int(input('Informe o terceiro numero: '))
+#verifica quem é menor
+menor = a
+if b < a and b<c:
+    menor=b
+if c < a and c<b:
+    menor = c
+#verificando Maior
+if b > a and b > c:
+    maior = b
+if c> a and c>b:
+    maior= c
+print ( 'MAior é {}'.format (maior))
+print ( 'Menor é {}'.format (menor))
+
