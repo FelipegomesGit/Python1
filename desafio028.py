d013aff14d58c5857bd532ef59a4f8b863743daf
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-18 18:53:10 -0300

    resolvido

--- desafio028.py
+++ desafio028.py
@@ -1,8 +1,19 @@
 # peça para digitar um numero de 0 até 5 e o usuário tenta descobrir qual numero o computador escolheu
 from random import randint
-num = randint (0, 5) # escolhe numero de 0 até 5
+num = randint(0, 5) # escolhe numero de 0 até 5
 jogador=int(input('Digite um numero entre 0 a 5: '))
-if jogador == num:     
-    print ('Parabéns você acertou o  numero {}'. format (num))
+if jogador == (num):   
+    print ('Parabéns você acertou o  numero {}'.format(jogador))
 else:
-print ('Você errou o numero')
+    print ("Errouuuuu!!!!")
+'''
+import random
+nums = [0, 1, 2, 3, 4, 5]
+np = random.choice(nums)
+print('Vou pensar em um número de 0 a 5... Tente adivinhar qual é o número!')
+
+adivinhe = int(input('Qual é o número? '))
+if adivinhe == (np):
+    print('VOCÊ ACERTOU, PARABÉNS!')
+else:
+    print('O Computador VENCEU!')'''
