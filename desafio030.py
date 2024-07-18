156e37cea18ce97d233a57de0358a8fb8e14d2ad
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-18 19:21:02 -0300

    desafio 30

---
+++ desafio030.py
@@ -0,0 +1,7 @@
+# crie um programaa que mostre se ele é par ou impar.
+num=int(input('Digite um número: ' ))
+if num%2 == 0:
+    print('Seu número {} é par!'.format(num))
+else:
+    print('Esse numero é impar')
+              
