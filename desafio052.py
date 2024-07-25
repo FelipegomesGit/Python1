f0336897f9b6769a3d094d675451b9790cba617c
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-25 17:35:49 -0300

    commit
    numeros primo

--- desafio052.py
+++ desafio052.py
@@ -2,8 +2,16 @@
 num = int (input('Digite um numero para saber se ele é primo: '))
 p=0
 d=0
-for c in range (1, num):
-    p = num % 3
-    d = num % 5
-    if p == 0 and d==0:
-        print (c)
+for c in range (1, num +1):      
+          if (num % c) == 0:
+             print('\033[33m', end='')
+             p += 1
+          else:
+             print('\033[31m', end='')  
+          print('{} '.format(c), end='')  
+print ('\n O numero {} foi divisivel {} vezes'.format(num, p))
+if p ==2:
+     print('E por isso ele é primo')
+else:
+     print ('E por isso não é primo')
+            
