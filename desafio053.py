4ba6a81f7b20fbebbda57c322bd3903ef58f97e0
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-26 14:52:20 -0300

    commit

--- desafio053.py
+++ desafio053.py
@@ -1,5 +1,12 @@
 # palindromo (frases de tras para frente)
-frase = str (input ('Digite uma frase para eu confirmar se é um palindromo: ')).strip().upper()
-print (frase)
-f=len(frase.split())
-print (f)
+import re
+frase = str (input ('Digite uma frase para eu confirmar se é um palindromo: ')).strip()
+texto = re.sub(r"[^\w\s]", "", frase.lower()) #utilizamos a biblioteca re para remover todos os caracteres que não são letras ou espaços. Em seguida, convertemos a string para letras minúsculas e realizamos a comparação.
+if frase == texto[::-1]: # faz o inverso da frase
+        print ('Essa frase é um palindromo')
+else:
+        print ('Não é um palindromo')
+          
+    
+        
+
