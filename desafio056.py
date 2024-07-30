44b9d8647519959e17e6fdfb97d8e0dcb1cff389
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-07-29 21:10:27 -0300

    commit

--- desafio056.py
+++ desafio056.py
@@ -1,2 +1,6 @@
 # um programa que peça nome, idade e sexo de 4 pessoas
-#mosntre a media das idades, qual o nome do homem mais velho e quantas mudelhes tes menos de 20 anos
+#mosntre a media das idades, qual o nome do homem mais velho e quantas mulheres tem menos de 20 anos
+for c in range (0, 4):
+    nome = str (input("Digite o nome da {} pessoa: ".format(c))).strip()
+    idade = int (input("Digite a idade da {} pessoa: ".format(c)))
+    sexo = int (input('Informe o seu genero e F: [M, F]'))
