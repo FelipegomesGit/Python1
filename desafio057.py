ce62a7b65c85be5a82776c86bf11755c66ddd03a
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-08-02 18:25:48 -0300

    commit

---
+++ desafio057.py
@@ -0,0 +1,10 @@
+# faça um programa que leia o sexo de uma pessoa,  mas só aceite os valores de 'M' ou "F". caso esteja errado peça digitação novamente.
+sexo = ' '
+while sexo != 'F' and sexo !='M':
+    sexo = str (input ('Digite corretamente seu sexo: [F/M]: ')).upper()
+    
+         
+print ('Você digitou o caractere aceito')
+
+                
+                
