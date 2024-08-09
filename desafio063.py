9cdb7f771b8a3247fa71ed3eefc6507fa2325b67
FELIPE SILVA SOARES GOMES <felipessgomes10@gmail.com>
2024-08-09 17:32:35 -0300

    commit

--- desafio063.py
+++ desafio063.py
@@ -1,10 +1,15 @@
 # sequencia de fibonacci
 num = int (input ('Informe os numero de elementos da sequencia de fibonacci que deseja: '))
-f = 1
-
-f = f + (1*f)
-print (f)
-'''while f <= num:
-    print(f)'''
-    
-    
+f = 0
+f2 = 1
+f3 = 1
+c = 3
+print(f)
+print(f2)
+while c <= num:
+    f3 = f + f2
+    print (f3)
+    f=f2
+    f2=f3
+    c += 1
+print ('FIM')
